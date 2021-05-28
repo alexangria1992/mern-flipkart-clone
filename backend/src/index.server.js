11dfe1267d299cf9c1ecbcf71dff88cb84a07be0
alexangria1992 <alex040892@gmail.com>
2021-05-28 14:05:30 +1200

    installed nodemon

--- backend/src/index.server.js
+++ backend/src/index.server.js
@@ -6,11 +6,21 @@ const app = express();
 
 env.config();
 
+
 app.get("/", (req, res, next) => {
     res.status(200).json({
         message: "Hello from Server"
     })
 })
+
+app.post("/data", (req, res, next) => {
+    res.status(200).json({
+        message: req.body
+    })
+})
+
+
+
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`)
 })
