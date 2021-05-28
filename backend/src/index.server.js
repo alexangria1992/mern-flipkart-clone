4e7a4f11dd4bc6ee132403d0681c88a9e8328c15
alexangria1992 <alex040892@gmail.com>
2021-05-28 14:49:11 +1200

    Connected MongoDB

--- backend/src/index.server.js
+++ backend/src/index.server.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const env = require("dotenv");
 const app = express(); 
-
+const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
 //environment variable or you can say constants 
 
 env.config();
 
+//mongodb connection 
+//mongodb+srv://root:<password>@cluster0.i0vcr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+mongoose.connect(
+    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.i0vcr.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`,
+     {
+         useNewUrlParser: true, 
+         useUnifiedTopology: true
+    }).then(() => {
+        console.log("Database Connected!!")
+    });
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 app.get("/", (req, res, next) => {
     res.status(200).json({
