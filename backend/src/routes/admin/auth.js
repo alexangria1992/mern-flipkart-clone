51a4c8d2df29e86af68814eb2f1d4cebf288343e
alexangria1992 <alex040892@gmail.com>
2021-06-04 19:20:51 +1200

    API request validation

--- backend/src/routes/admin/auth.js
+++ backend/src/routes/admin/auth.js
@@ -1,11 +1,11 @@
 const express = require("express")
 const router = express.Router()
 const {signup, signin, requireSignin} = require("../../controller/admin/auth")
+const {validateSignupRequest, isRequestValidated, validateSigninRequest} = require("../../validators/auth")
 
+router.post("/admin/signup", validateSignupRequest, isRequestValidated, signup)
 
-router.post("/admin/signup", signup)
-
-router.post("/admin/signin", signin)
+router.post("/admin/signin", isRequestValidated, validateSigninRequest, signin)
 
 
 
