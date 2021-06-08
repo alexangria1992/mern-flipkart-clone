82ad8d52bcb9ee7f219641f79305590cbbb6f885
alexangria1992 <alex040892@gmail.com>
2021-06-08 21:34:48 +1200

    Fix Cart Api & Update Category Api

--- backend/src/index.server.js
+++ backend/src/index.server.js
@@ -3,6 +3,7 @@ const env = require("dotenv");
 const app = express(); 
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
+const path = require("path")
 //environment variable or you can say constants 
 
 //routes 
@@ -29,6 +30,7 @@ mongoose.connect(
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
+app.use("/public", express.static(path.join(__dirname, 'uploads')))
 app.use("/api", authRoutes);
 app.use("/api", adminRoutes);
 app.use("/api", categoryRoutes);
